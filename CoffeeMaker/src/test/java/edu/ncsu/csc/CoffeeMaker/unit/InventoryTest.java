7ea22a9b0b40e85fac438ecf0951faa67b767762
ulemj munkhtur <umunkhtur@bowdoin.edu>
2024-03-07 10:47:35 -0500

    Apply changes from teaching staff

--- CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/unit/InventoryTest.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/unit/InventoryTest.java
@@ -35,6 +35,31 @@ public class InventoryTest {
         inventoryService.save( ivt );
     }
     
+    @Test
+    @Transactional
+    public void testConsumeInventory () {
+        final Inventory i = inventoryService.getInventory();
+
+        final Recipe recipe = new Recipe();
+        recipe.setName( "Delicious Not-Coffee" );
+        recipe.setChocolate( 10 );
+        recipe.setMilk( 20 );
+        recipe.setSugar( 5 );
+        recipe.setCoffee( 1 );
+
+        recipe.setPrice( 5 );
+
+        i.useIngredients( recipe );
+
+        /*
+         * Make sure that all of the inventory fields are now properly updated
+         */
+
+        Assertions.assertEquals( 490, (int) i.getChocolate() );
+        Assertions.assertEquals( 480, (int) i.getMilk() );
+        Assertions.assertEquals( 495, (int) i.getSugar() );
+        Assertions.assertEquals( 499, (int) i.getCoffee() );
+    }
 
     @Test
     @Transactional

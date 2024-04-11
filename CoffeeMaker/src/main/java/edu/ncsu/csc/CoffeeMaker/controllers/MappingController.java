eb27545577baa79dd3d39a216ead33c55911fa83
charlie-apolseed <capolinsky@bowdoin.edu>
2024-04-11 19:39:05 -0400

    [Implementation] Corrected route for MappingControler Add Recipe

--- CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/MappingController.java
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/MappingController.java
@@ -37,7 +37,7 @@ public class MappingController {
      */
     @GetMapping ( { "/recipe", "/recipe.html" } )
     public String addRecipePage ( final Model model ) {
-        return "recipe";
+        return "addRecipePopulated";
     }
 
     /**

cd377bff8c308d948c9503e064dbec8031fedc18
charlie-apolseed <capolinsky@bowdoin.edu>
2024-04-18 12:02:43 -0400

    [Implementation] Added APIIngredientController for Workshop 4 Part 2

---
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/APIIngredientController.java
@@ -0,0 +1,8 @@
+package edu.ncsu.csc.CoffeeMaker.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIIngredientController extends APIController{
+
+}

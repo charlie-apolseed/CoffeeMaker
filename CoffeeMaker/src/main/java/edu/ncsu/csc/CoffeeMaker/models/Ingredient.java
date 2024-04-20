c3e4b56b958a66b9f0234ee02a682a63acb90448
TaviDG <tavi@elyandpilar.com>
2024-04-19 20:34:01 -0400

    [Implementation] Refactored Ingredient class Changed class to have a name field rather than an ingredient type and added amount methods.

--- CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Ingredient.java
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Ingredient.java
@@ -1,59 +1,117 @@
 package edu.ncsu.csc.CoffeeMaker.models;
 
-import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import edu.ncsu.csc.CoffeeMaker.models.enums.IngredientType;
+
 
 @Entity
 public class Ingredient extends DomainObject {
+	
+	
 	@Id
 	@GeneratedValue
 	Long id;
-	@Enumerated ( EnumType.STRING )
-	IngredientType ingredient;
+	
+	
+	String name;
+	
+	
 	Integer amount;
 
-	public Ingredient(IngredientType ingredient, Integer amount) {
+	/**
+	 * Constructor sets fields
+	 * 
+	 * @param name to set
+	 * @param amount to set 
+	 */
+	public Ingredient(String name, Integer amount) {
 		super();
-		this.ingredient = ingredient;
+		this.name = name;
 		this.amount = amount;
 	}
 	
+	/**
+	 * Empty constructor for Hibernate
+	 */
 	public Ingredient() {
 		
 	}
 
 	@Override
 	public String toString() {
-		return "Ingredient [id=" + id + ", ingredient=" + ingredient + ", amount=" + amount + "]";
+		return "Ingredient [id=" + id + ", ingredient=" + name + ", amount=" + amount + "]";
 	}
 
-	public IngredientType getIngredient() {
-		return ingredient;
-	}
 
-	public void setIngredient(IngredientType ingredient) {
-		this.ingredient = ingredient;
-	}
 
+	/**
+	 * 
+	 * @return amount
+	 */
 	public Integer getAmount() {
 		return amount;
 	}
 
+	/**
+	 * Setter for amount
+	 * 
+	 * @param amount to set
+	 * @throws IAE if amount is negative
+	 */
 	public void setAmount(Integer amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("Amount cannot be negative");
+		}
 		this.amount = amount;
+	}
+	
+	/**
+	 * Adds the given int to amount
+	 * 
+	 * @param addAmount amount to add
+	 */
+	public void addAmount(int addAmount) {
+		this.amount += addAmount;
+	}
+	
+	/**
+	 * Removes the given int from amount 
+	 * 
+	 * @param removeAmount amount to remove
+	 */
+	public void removeAmount(int removeAmount) {
+		this.amount -= removeAmount;
+	}
+
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets name 
+	 * 
+	 * @param name to set
+	 * @throws IAE if the name is empty or null
+	 */
+	public void setName(String name) {
+		
+		if (name == null || "".equals(name)) {
+			throw new IllegalArgumentException("Name cannot be null or empty");
+		}
+		
+		this.name = name;
 	}
 
 	@Override
-	public Serializable getId() {
-		// TODO Auto-generated method stub
-		return null;
+	public Long getId() {
+		return id;
 	}
 	
 
